cee831a153591f089c2d00dc08842d35bd273427
KAWACHI Takashi <tkawachi@gmail.com>
2014-08-11 23:15:59 +0900

    Comments

--- sslreminder.go
+++ sslreminder.go
@@ -6,15 +6,15 @@ It can be configured via environmental variables.
 
 Followings are mandatory.
 
-* HOSTS for comma separated hosts to be checked.
-* EMAILS for comma separated email addresses.
-* SENDGRID_USERNAME for SendGrid user name.
-* SENDGRID_PASSWORD for SendGrid password.
+	* HOSTS for comma separated hosts to be checked.
+	* EMAILS for comma separated email addresses.
+	* SENDGRID_USERNAME for SendGrid user name.
+	* SENDGRID_PASSWORD for SendGrid password.
 
 Followings are optional.
 
-* THRESHOLD_DAYS for threshold remaining days to remind. (default 30)
-* FROM for from address. (default the first address in EMAILS)
+	* THRESHOLD_DAYS for threshold remaining days to remind. (default 30)
+	* FROM for from address. (default the first address in EMAILS)
 
 It checks expiration dates once a day. It sends reminder via email
 if any of certificates expire within THRESHOLD_DAYS.
@@ -46,6 +46,7 @@ type sendgridConfig struct {
 	password string
 }
 
+// Get expiration date for given host.
 func GetExpiration(host string) (expiration time.Time, err error) {
 	conn, err := tls.Dial("tcp", host+":443", &tls.Config{})
 	if err != nil {
@@ -70,6 +71,8 @@ func GetExpiration(host string) (expiration time.Time, err error) {
 	return
 }
 
+// Read an environmental variable.
+// Exit process if it's empty or not set.
 func envMandatory(key string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
@@ -78,6 +81,8 @@ func envMandatory(key string) string {
 	return value
 }
 
+// Read an environmental variable.
+// Returns defualtValue if it's empty or not set.
 func envOptional(key string, defaultValue string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
@@ -86,6 +91,7 @@ func envOptional(key string, defaultValue string) string {
 	return value
 }
 
+// Read SendGrid related configs.
 func readSendgridConfig() *sendgridConfig {
 	return &sendgridConfig{
 		envMandatory("SENDGRID_USERNAME"),
@@ -93,6 +99,7 @@ func readSendgridConfig() *sendgridConfig {
 	}
 }
 
+// Read general config.
 func readConfig() *config {
 	DEFAULT_THRESHOLD_DAYS := "30"
 	thresholdString := envOptional("THRESHOLD_DAYS", DEFAULT_THRESHOLD_DAYS)
@@ -112,6 +119,7 @@ func readConfig() *config {
 	}
 }
 
+// Get a map from hosts to expiration dates.
 func GetExpirationMap(config *config) map[string]time.Time {
 	expirationMap := make(map[string]time.Time, len(config.hosts))
 
@@ -130,6 +138,7 @@ func GetExpirationMap(config *config) map[string]time.Time {
 	return expirationMap
 }
 
+// Check ssl certificates for given hosts, then remind if necessary.
 func check(config *config, sgConfig *sendgridConfig, now time.Time) {
 	log.Println("Check started")
 	exMap := GetExpirationMap(config)
@@ -148,6 +157,7 @@ func check(config *config, sgConfig *sendgridConfig, now time.Time) {
 	log.Println("Check finished")
 }
 
+// A body of remind mail
 func mailBody(config *config, now time.Time, exMap map[string]time.Time) string {
 	threshold := now.AddDate(0, 0, config.thresholdDays)
 	soon := make(map[string]time.Time)
@@ -177,6 +187,7 @@ func mailBody(config *config, now time.Time, exMap map[string]time.Time) string 
 	return buf.String()
 }
 
+// Remind via email.
 func remind(config *config, sgConfig *sendgridConfig, now time.Time,
 	exMap map[string]time.Time) {
 	sg := sendgrid.NewSendGridClient(sgConfig.username, sgConfig.password)
